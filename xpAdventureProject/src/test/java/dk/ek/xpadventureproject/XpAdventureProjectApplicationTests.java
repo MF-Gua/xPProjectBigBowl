7c4f8703e8b9d4a3da5eb46560d038dc0a0bb986
MF-Gua <tobiasagerbek6@gmail.com>
2026-02-27 08:44:00 +0100

    Add files via upload

---
+++ xpAdventureProject/src/test/java/dk/ek/xpadventureproject/XpAdventureProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package dk.ek.xpadventureproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XpAdventureProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
