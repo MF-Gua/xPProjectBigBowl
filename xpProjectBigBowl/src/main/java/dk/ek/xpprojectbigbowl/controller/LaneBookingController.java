5df85921a8b9eb3164b966f47421be06e1eadd81
JB4000 <jabi0002@stud.ek.dk>
2026-03-04 09:37:55 +0100

    1.3 made html for laneboooking. Adjusted index buttons and their links. Adjusted controller for lanebooking

--- xpProjectBigBowl/src/main/java/dk/ek/xpprojectbigbowl/controller/LaneBookingController.java
+++ xpProjectBigBowl/src/main/java/dk/ek/xpprojectbigbowl/controller/LaneBookingController.java
@@ -2,12 +2,13 @@ package dk.ek.xpprojectbigbowl.controller;
 
 import dk.ek.xpprojectbigbowl.model.LaneBooking;
 import dk.ek.xpprojectbigbowl.Service.LaneBookingService;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RestController
-@RequestMapping("/bookings")
+@Controller
+//@RequestMapping("/bookings")
 public class LaneBookingController {
 
     private final LaneBookingService service;
@@ -16,18 +17,25 @@ public class LaneBookingController {
         this.service = service;
     }
 
-    @PostMapping
-    public LaneBooking createBooking(@RequestBody LaneBooking booking) {
-        return service.createBooking(booking);
-    }
 
-    @GetMapping
-    public List<LaneBooking> getAllBookings() {
-        return service.getAllBookings();
+    @GetMapping("/lanes")
+    public String showLaneBookingForm() {
+        return "bookbane";
     }
-
-    @GetMapping("/{id}")
-    public LaneBooking getBooking(@PathVariable Long id) {
-        return service.getBookingById(id);
-    }
+//
+//
+//    @PostMapping
+//    public LaneBooking createBooking(@RequestBody LaneBooking booking) {
+//        return service.createBooking(booking);
+//    }
+//
+//    @GetMapping
+//    public List<LaneBooking> getAllBookings() {
+//        return service.getAllBookings();
+//    }
+//
+//    @GetMapping("/{id}")
+//    public LaneBooking getBooking(@PathVariable Long id) {
+//        return service.getBookingById(id);
+//    }
 }
